4b46d82664c6c13b169d8ce8f3c3c58f9ebb38a0
Alberto Monteiro <alberto.costa.monteiro@gmail.com>
2024-12-15 18:47:07 -0300

    Configuração da parte de segurança

--- src/main/java/com/rocksti/miniautorizador/MiniAutorizadorApplication.java
+++ src/main/java/com/rocksti/miniautorizador/MiniAutorizadorApplication.java
@@ -1,4 +1,4 @@
-package com.rocksti.mini_autorizador;
+package com.rocksti.miniautorizador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/rocksti/miniautorizador/MiniAutorizadorApplicationTests.java
+++ src/test/java/com/rocksti/miniautorizador/MiniAutorizadorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rocksti.mini_autorizador;
+package com.rocksti.miniautorizador;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
